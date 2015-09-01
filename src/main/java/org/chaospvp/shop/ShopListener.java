33c96d9e175f8bd0c047270f25217ac254d77948
vemacs <d@nkmem.es>
2015-09-01 16:27:23 -0600

    resolve right click dupe

--- src/main/java/org/chaospvp/shop/ShopListener.java
+++ src/main/java/org/chaospvp/shop/ShopListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -64,7 +65,13 @@ public class ShopListener implements Listener {
                     shouldClose = true;
                     e.setCancelled(true);
                     if (e.getAction() == InventoryAction.SWAP_WITH_CURSOR) {
-                        inv.addItem(e.getCursor());
+                        if (e.getClick() == ClickType.RIGHT) {
+                            ItemStack realItem = e.getCursor().clone();
+                            inv.addItem(realItem);
+                            e.getCursor().setAmount(1);
+                        } else {
+                            inv.addItem(e.getCursor());
+                        }
                         e.setCancelled(false);
                         e.setCurrentItem(null);
                     }
